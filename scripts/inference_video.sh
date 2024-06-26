c7537a77f2a9666c23c8ae78d53116933a65c0ab
BossunWang <vvmodouco@gmail.com>
2024-06-26 17:21:44 +0800

    test: modify test video path

--- scripts/inference_video.sh
+++ scripts/inference_video.sh
@@ -2,7 +2,7 @@ set -x
 
 CONFIG="configs/halpe_26/resnet/256x192_res50_lr1e-3_1x.yaml"
 CKPT="pretrained_models/halpe26_fast_res50_256x192.pth"
-VIDEO="/media/bossun/新增磁碟區/Datasets/DanceDatasets/DanceDatasets_video/delight/delight_006_20231017_0#0-32#0.mp4"
+VIDEO="/workspace/Datasets/DanceDatasets/DanceDatasets_part_video/delight_026_20231017_0#0-213#0p6.mp4"
 OUTDIR=${4:-"./examples/res_video"}
 
 python scripts/demo_inference.py \
